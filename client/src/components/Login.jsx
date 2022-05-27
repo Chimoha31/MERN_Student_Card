50a0c81133d74bdf5427fce09de7555f234d84b2
Chimoha31 <mokochii1108@gmail.com>
2022-05-27 07:03:58 -0700

    Login Signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,86 +3,48 @@ import GoogleButton from "react-google-button";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const handleSubmit = () => {
-
-  }
+  const handleSubmit = () => {};
 
   return (
-    <div>
-      <div className="p-4 flex flex-col justify-center items-center">
-        <h2 className="mb-3">Firebase Auth Login</h2>
-        <form onSubmit={handleSubmit} className="w-64 border bg-blue-50" >
-            <input
-              type="email"
-              placeholder="Email address"
-              className="block"
-              // onChange={(e) => setEmail(e.target.value)}
-              />
+    <div className="w-full h-screen p-4 flex flex-col justify-center items-center">
+      <div className="w-auto py-3 px-5 flex flex-col justify-center items-center border border-r-emerald-200">
+        <h2 className="mb-3"> Login</h2>
+        <form onSubmit={handleSubmit} className="">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="block w-72 border border-stone-700 px-3 py-2 rounded-md mb-3"
+            // onChange={(e) => setEmail(e.target.value)}
+          />
 
-            <input
-              type="password"
-              placeholder="Password"
-              className="block"
-              // onChange={(e) => setPassword(e.target.value)}
-            />
-  
+          <input
+            type="password"
+            placeholder="Password"
+            className="block w-72 border border-stone-700 px-3 py-2 rounded-md mb-3"
+            // onChange={(e) => setPassword(e.target.value)}
+          />
 
           <div className="w-full text-center">
-            <button className="bg-sky-600" variant="primary" type="Submit">
+            <button className="bg-blue-500 w-72 px-3 py-2 rounded-md text-white" variant="primary" type="Submit">
               Log In
             </button>
           </div>
         </form>
-        <hr />
+
+      <hr className="border border-stone-900 w-72" />
+
         <div>
           <GoogleButton
-            className=""
+            className="!w-72"
             type="dark"
             // onClick={handleGoogleSignIn}
           />
         </div>
       </div>
+
       <div className="p-4 mt-3 text-center">
         Don't have an account? <Link to="/signup">Sign up</Link>
       </div>
-
-
-
-      {/* <form>
-        <label className="block">
-          <span className="block text-sm font-medium text-slate-700">
-            Username
-          </span>
-
-          <input
-            type="text"
-            value="tbone"
-            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    "
-          />
-        </label>
-        <label className="block">
-          <span className="block text-sm font-medium text-slate-700">
-            Email
-          </span>
-          <input
-            type="email"
-            className="peer mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    "
-          />
-          <p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-            Please provide a valid email address.
-          </p>
-        </label>
-      </form> */}
     </div>
   );
 };
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const handleSubmit = () => {};
 
   return (
-    <>
-     
-    </>
+    <div className="w-full h-screen p-4 flex flex-col justify-center items-center">
+      <div className="w-auto py-3 px-5 flex flex-col justify-center items-center border border-r-emerald-200">
+        <h2 className="mb-3">Sign Up</h2>
+        <form onSubmit={handleSubmit} className="">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="block w-72 border border-stone-700 px-3 py-2 rounded-md mb-3"
+            // onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="block w-72 border border-stone-700 px-3 py-2 rounded-md mb-3"
+            // onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="w-full text-center">
+            <button className="bg-blue-500 w-72 px-3 py-2 rounded-md text-white" variant="primary" type="Submit">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="p-4 mt-3 text-center">
+        Already have an account? <Link to="/">Login</Link>
+      </div>
+    </div>
   );
 };
 
 export default SignUp;
+
