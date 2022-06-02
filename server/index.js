d7d121ef0d273755e095a7a88b3c6f454bae4817
Chimoha31 <mokochii1108@gmail.com>
2022-06-02 11:09:55 -0700

    Fixed

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const StudentRoutes = require("./routes/student")
 const cors = require('cors');
-const PORT =  process.env.PORT || 4000;
+const PORT =  process.env.PORT || 5000;
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("./client/build"))
 
 const mongoURL = process.env.MONGODB_URL
 mongoose.connect(mongoURL, (err) => {
